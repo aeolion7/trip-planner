c42452019436da42743b7fab45e3191b120cfb54
Joseph Lionarons <lionsamurai@gmail.com>
2018-11-13 16:49:55 -0500

    fixed marker issue

--- src/marker.js
+++ src/marker.js
@@ -1,20 +1,20 @@
 const mapboxgl = require('mapbox-gl');
-const map = require('./index');
 
 function buildMarker(type, coordinates) {
   const markerDomEl = document.createElement('div');
   markerDomEl.style.width = '32px';
   markerDomEl.style.height = '39px';
+  let markerStyle;
 
   if (type === 'activity') {
-    markerDomEl.style.backgroundImage = 'url(http://i.imgur.com/WbMOfMl.png)';
+    markerStyle = 'url(http://i.imgur.com/WbMOfMl.png)';
+  } else if (type === 'hotel') {
+    markerStyle = 'url(http://i.imgur.com/D9574Cu.png)';
+  } else if (type === 'restaurant') {
+    markerStyle = 'url(http://i.imgur.com/cqR6pUI.png)';
   }
-  if (type === 'hotel') {
-    markerDomEl.style.backgroundImage = 'url (http://i.imgur.com/D9574Cu.png)';
-  }
-  if (type === 'restaurant') {
-    markerDomEl.style.backgroundImage = 'url (http://i.imgur.com/cqR6pUI.png)';
-  }
+
+  markerDomEl.style.backgroundImage = markerStyle;
 
   return new mapboxgl.Marker(markerDomEl).setLngLat(coordinates);
 }
